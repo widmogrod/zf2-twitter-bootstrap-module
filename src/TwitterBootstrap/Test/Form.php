fe4ecdf0705742190e285e6911286edea811d82f
Gabriel <widmogrod@gmail.com>
2011-11-07 23:20:54 +0100

    fix form

--- src/TwitterBootstrap/Test/Form.php
+++ src/TwitterBootstrap/Test/Form.php
@@ -9,7 +9,7 @@ class Form extends \TwitterBootstrap\Form\Form
 {
     public function init()
     {
-        $this->setMethodg(self::METHOD_POST);
+        $this->setMethod(self::METHOD_POST);
 
         $this->addElement('text', 'title', array(
             'label' => 'Title',
