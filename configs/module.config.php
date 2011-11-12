dc3d3a472eb3741c5e02759d03f26ff6d2ce999b
Gabriel <widmogrod@gmail.com>
2011-11-12 16:11:50 +0100

    update assetic-configuration for module

--- configs/module.config.php
+++ configs/module.config.php
@@ -17,6 +17,19 @@ return array(
             'assetic-configuration' => array(
                 'parameters' => array(
                     'config' => array(
+
+                        /**
+                         * Example, defaining that 'default' route will use twitter bootstrap css
+                         * Setup your application or module, to use this section, if required.
+                         */
+                        /** /
+                        'routers' => array(
+                            'default' => array(
+                                '@twitter_bootstrap_css'
+                            )
+                        ),
+                        /**/
+
                         'modules' => array(
                             'twitterbootstrap' => array(
                                 'root_path' => __DIR__ . '/../assets',
@@ -26,6 +39,8 @@ return array(
                                             'bootstrap.css'
                                         )
                                     ),
+
+                                    # todo
 //                                    'twitter_bootstrap_js' => array(
 //                                        'assets' => array(
 //                                            'http://code.jquery.com/jquery-1.5.2.min.js',
